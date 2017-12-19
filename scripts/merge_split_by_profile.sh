75fa7a1d626e6c130602261061bdf897e148b941
Florent Devillard <fdevillard@me.com>
2017-12-19 13:01:51 +0100

    Added script for merging split-by-profile

---
+++ scripts/merge_split_by_profile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read_path='../data/personalized-dialog-dataset/split-by-profile/'
+output_path='../data/personalized-dialog-dataset/merged-from-split-by-profile2/'
+type_files_to_copy='dev trn tst-OOV tst'
+base_file_name='personalized-dialog-task5-full-dialogs'
+extension='txt'
+
+mkdir -p $output_path
+
+for type in $type_files_to_copy; do
+    name_mask="$type.$extension"
+    output_file="$base_file_name-$type.$extension"
+    find $read_path -name "*-$name_mask" | xargs cat > $output_path/$output_file
+done;
